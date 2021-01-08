0cdeaa64666a74f65ee96b2439206be49c4496cf
vu_trang155 <tranghoangphongvu@gmail.com>
2021-01-08 19:01:10 +0100

    preprocessing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from src.preprocessing import ReadTweet, Preprocessing
+from src.preprocessing import ReadTweet, PreprocessingText
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
 # A modifier, ça dépend de votre workdir, pour moi c'est 'sentiments-tweets'. Si vous êtes
 # à 'sentiments-tweets/src' => '../data/eit_annot_100_1899.txt"
@@ -6,14 +9,33 @@ FILEPATH = "data/eit_annot_100_1899.txt"
 
 # Get tweets
 X, y = ReadTweet(FILEPATH).get_data()
-n_tweets = y.shape[0]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, shuffle=True, test_size=0.3, random_state=43)
 
+#%% PREPROCESSING :
 # Il dispose un degré d'importance pour chaque tâche spécifique :
 #   1. lowercase, noise_removal (priorité)
 #   2. normalization
 #   3. stopword_removal, lemmatization
-tasks = Preprocessing.DEFAULT_TASKS # lowercase, noise_removal, normalization, stopword_removal, lemmatization
-preprocessing = Preprocessing(X, tasks=tasks, model="bow")
-X_lowercased = preprocessing.get()
+''' Equivalent : 
+#%% Preprocessing TEXT
+tasks = PreprocessingText.DEFAULT_TASKS # lowercase, noise_removal, normalization, stopword_removal, lemmatization
+preprocessing = PreprocessingText(tasks=tasks, keep_tags=True)
+X_normalized = preprocessing.fit_transform(X)
 
-
+#%% Extracting features from Texts
+count_vect = CountVectorizer()
+X_counts = count_vect.fit_transform(X_normalized)
+print(X_counts.shape)
+# Transformer en fréquence [0,1] pour éviter la différences dans la longueur entre les tweets longs et les tweets courts
+tf_transformer = TfidfTransformer(use_idf=False)
+X_tf = tf_transformer.fit_transform(X_counts)
+print(X_tf.shape)
+'''
+tasks = PreprocessingText.DEFAULT_TASKS # lowercase, noise_removal, normalization, stopword_removal, lemmatization
+preprocessing_pipeline = Pipeline([
+    ('preprocesing_text', PreprocessingText(tasks=tasks, keep_tags=True)),
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer())
+])
+X_train = preprocessing_pipeline.fit_transform(X_train)
+X_valid = preprocessing_pipeline.fit_transform(X_valid)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,10 @@
 import numpy as np
 import re
+
 from utils import string_utils
+import constants
+
+from sklearn.base import BaseEstimator, TransformerMixin
 
 # ReadTweet
 class ReadTweet :
@@ -35,105 +39,124 @@ class ReadTweet :
         '''
         return self.tweets, self.results
 
-class Preprocessing :
-    '''
+class PreprocessingText (BaseEstimator, TransformerMixin):
+    """
     Préprocessing des données
-    '''
+    """
     DEFAULT_TASKS = ["lowercase", "noise_removal", "normalization", "stopword_removal", "lemmatization"]
     MODELS = ["bow", "tf-idf"]
 
-    def __init__(self, X, tasks=DEFAULT_TASKS, model="bow", keep_tags=False):
-        # TODO : making model param an object !
+    def __init__(self, tasks=DEFAULT_TASKS, keep_tags=True):
         '''
-        Initialise la préprocessing
-        :param X: Tweets à préprocessing
+        Initialiser la préprocessing
+        :param tasks: list, Les tachês à passer. Par défaults, elles sont définies dans le variable constant DEFAULT_TASKS
+        :param keep_tags: boolean, True si l'on veut garder les hashtags et les mentions tags
         '''
-
-        self.X = X
-
         self.keep_tags = keep_tags
+        self.tasks = tasks
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, **transform_params):
         ########## TASKS ##############
         ###############################
         # MUST
         ########################
-        if "lowercase" in tasks:
-            self.lowercase()
-        if "noise_removal" in tasks:
-            self.noise_removal()
+        if "lowercase" in self.tasks:
+            X = self.lowercase(X)
+        #########################
+        if "normalization" in self.tasks:
+            X = self.normalization(X)
+        #########################
+        if "noise_removal" in self.tasks:
+            X = self.noise_removal(X)
         #########################
 
-        # SHOULD
-        #########################
-        if "normalization" in tasks:
-            self.normalization()
-        #########################
 
         # DEPEND
         #########################
-        if "stopword_removal" in tasks:
-            self.stopword_removal()
-        if "lemmatization" in tasks:
-            self.lemmatization()
+        if "stopword_removal" in self.tasks:
+            X = self.stopword_removal(X)
+        if "lemmatization" in self.tasks:
+            X = self.lemmatization(X)
         #########################
+        return X
 
-
-        ########## MODEL ##############
-        ###############################
-        if model == "bow":
-            # TODO
-            pass
-        elif model == "tf-idf":
-            # TODO
-            pass
-
-
-    def lowercase(self):
+    def lowercase(self, X):
         '''
-        Rendre les tweets en miniscule
+            Rendre les tweets en miniscule
+        :param X: ndarray de dimension (n,)
+        :return: ndarray de dimension (n,) : tweets en miniscule
         '''
-        self.X = np.char.lower(self.X)
+        return np.char.lower(X)
 
-    def noise_removal(self):
+    def normalization(self, X):
         '''
-        Enlever les caractères spéciaux collés à un token .I.E. ":)hello" => ":) hello"
+        Normaliser les textes ("http://..." -> "##WEBSITE##")
+        Si deux emojis sont collés, on les séparent par une espace
+        :param X: ndarray (n, )
+        :return: ndarray (n,) : tweets normalisés
         '''
-        for i in range(self.X.shape[0]):
-            self.X[i] = string_utils.decontract(self.X[i])
+        for i in range(X.shape[0]):
+            X[i] = re.sub(r'http\S+', '##WEBSITE##', X[i])
+        return X
+
+    def noise_removal(self, X):
+        '''
+            Enlever les caractères spéciaux collés à un token .I.E. ":)hello" => ":) hello"
+        :param X: ndarray de dimension (n,)
+        :return: ndarray de dimension (n,) : Tweets où les bruits sont enlevés
+        '''
+        for i in range(X.shape[0]):
+            X[i] = string_utils.decontract(X[i])
 
             # TODO: ":)hello" to ":) hello"
             #   N'oublie pas keep_hashtags, keep_mention_tags.
             #   Si keep_hashtags = False => :)#Hello = :) # Hello
             #   Si non                   => :)#Hello = :) #Hello
+            prefix = ""
             if self.keep_tags == True:
-                #l = re.findall(r"[A-Za-z@#]+|\S", self.X[i])
-                pass
+                prefix = "@#"
+            # ATTENTION : Ce code n'a pas marché pour les langues étrangères
+            # Ceci ne marche que sur le code ASCII
+            #l = re.findall(r"[A-Za-z@#]+|\S", X[i])
+            # Resolution :
+            new_str = ""
+            for w in X[i].split():
+                # Decomposer en lsite
+                l = re.findall(r"[_" + prefix + constants.REGEX_UNICODE_WORD + r"]+|\S", w) # Exemple : "Hello:3#Mynameis" renvoie ["Hello" ":", "3", "#Mynameis"]
 
-        pass
+                # Rematch en string
+                # ["Hello" ":", "3", "#Myanmeis"] => "Hello :3 #Mynameis"
+                new_substr = ""
+                for element in l:
+                    # Check if element in list is a word (or hashtag, ...)
+                    if bool(re.match(r"[_" + prefix + constants.REGEX_UNICODE_WORD + r"]+", element)):
+                        new_substr = new_substr + " " + element + " "
+                    else :
+                        new_substr = new_substr + element
 
-    def normalization(self):
-        '''
-        Normaliser les textes (":)" -> "smiley")
-        '''
-        # TODO
-        pass
+                # Replace w by new_str
+                new_str = new_str+new_substr
+            X[i] = new_str
+        return X
 
-    def stopword_removal(self):
+
+    def stopword_removal(self, X):
         '''
         Supprimer les stop-words
+        :param X: ndarray (n, )
+        :return: ndarray (n,) : tweets ou les stopwords sont supprimer
         '''
         # TODO
-        pass
+        return X
 
-    def lemmatization(self):
+    def lemmatization(self, X):
         '''
-        Mettre les mots en forme initative
+        Mettre les mots en forme infinitive
+        :param X: ndarray (n, )
+        :return: ndarray (n,) : tweets en forme infinitive
         '''
         # TODO
-        pass
-
-    def get(self):
-        '''
-        Renvoie les données traitées
-        :return: X
-        '''
-        return self.X
+        return X
